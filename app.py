fd55b603cb79a32daf38cbedc667879d90766d1d
DHANAVISHNU A <83368841+dhanavishnu13@users.noreply.github.com>
2023-04-24 23:06:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
-st.title('Hello')
+#title 
+st.title('Text Detection & Layout Analysis')
+#uploading file
+file = st.file_uploader(label='Upload your document here',type=['png','jpg','jpeg','pdf'])
+button = st.button('Confirm')
+if button and file is not None:
+    if file.type=="image/png" or file.type=="image/jpg" or file.type=="image/jpeg":
+      parserIMG(file)
+      text_detection(file)
+    elif file.type == "application/pdf":
+      parserPDF(file)
